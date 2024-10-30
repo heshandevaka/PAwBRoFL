a82adfb4a00d1115119fbe615a1c44173a754879
heshandevaka <heshandevaka@gmail.com>
2024-10-30 14:12:22 -0400

    Update train script

--- train.sh
+++ train.sh
@@ -7,8 +7,8 @@ round=50
 agg=ex_noregret  #"average" "bulyankrum" "bulyanmedian" "bulyantrimmedmean" "ex_noregret" "filterl2" "krum" "median" "mom_ex_noregret" "mom_filterl2" "trimmedmean"
 mu=0.01
 
-# # without PA
-# python src/simulate.py --dataset=$dataset --attack=$attack --agg=$agg --round=$round --variable_local_size
+# without PA
+python src/simulate.py --dataset=$dataset --attack=$attack --agg=$agg --round=$round --variable_local_size
 
 # with PA
 python src/simulate.py --dataset=$dataset --attack=$attack --agg=$agg --round=$round --variable_local_size --pa --mu=$mu
